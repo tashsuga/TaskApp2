53d1eba88ad0a2cf95be3f9f9765f4e65d29c2ad
takeshisugai <tsu.vasco@gmail.com>
2019-01-26 15:44:45 +0900

    1/26

---
+++ app/src/main/java/com/onespan/takeshisugai/taskapp2/InputActivity.kt
@@ -0,0 +1,12 @@
+package com.onespan.takeshisugai.taskapp2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class InputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
