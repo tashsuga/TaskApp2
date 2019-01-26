e2e6402c21d86a31f89619beaddb1d5dfb1e8b45
takeshisugai <tsu.vasco@gmail.com>
2019-01-26 16:18:59 +0900

    1/26

--- app/src/main/java/com/onespan/takeshisugai/taskapp2/MainActivity.kt
+++ app/src/main/java/com/onespan/takeshisugai/taskapp2/MainActivity.kt
@@ -9,8 +9,83 @@ import io.realm.RealmChangeListener
 import io.realm.Sort
 import java.util.*
 
-import java.util.*
 
+class MainActivity : AppCompatActivity() {
+    private lateinit var mRealm: Realm
+    private val mRealmListener = object : RealmChangeListener<Realm> {
+        override fun onChange(element: Realm) {
+            reloadListView()
+        }
+    }
+
+    private lateinit var mTaskAdapter: TaskAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        // Realmの設定
+        mRealm = Realm.getDefaultInstance()
+        mRealm.addChangeListener(mRealmListener)
+
+        // ListViewの設定
+        mTaskAdapter = TaskAdapter(this@MainActivity)
+
+        // ListViewをタップしたときの処理
+        listView1.setOnItemClickListener { parent, view, position, id ->
+            // 入力・編集する画面に遷移させる
+        }
+
+        // ListViewを長押ししたときの処理
+        listView1.setOnItemLongClickListener { parent, view, position, id ->
+            // タスクを削除する
+            true
+        }
+
+        // アプリ起動時に表示テスト用のタスクを作成する
+        addTaskForTest()
+
+        reloadListView()
+    }
+
+    private fun reloadListView() {
+        // Realmデータベースから、「全てのデータを取得して新しい日時順に並べた結果」を取得
+        val taskRealmResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
+
+        // 上記の結果を、TaskList としてセットする
+        mTaskAdapter.taskList = mRealm.copyFromRealm(taskRealmResults)
+
+        // TaskのListView用のアダプタに渡す
+        listView1.adapter = mTaskAdapter
+
+        // 表示を更新するために、アダプターにデータが変更されたことを知らせる
+        mTaskAdapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mRealm.close()
+    }
+
+    private fun addTaskForTest() {
+        val task = Task()
+        task.title = "作業"
+        task.contents = "プログラムを書いてPUSHする"
+        task.date = Date()
+        task.id = 0
+        mRealm.beginTransaction()
+        mRealm.copyToRealmOrUpdate(task)
+        mRealm.commitTransaction()
+    }
+}
+
+/*
 
 class MainActivity : AppCompatActivity() {
 
@@ -51,19 +126,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    /*
-    teinit var mRealm: Realm
-
-    private val mRealmListener = object : RealmChangeListener<Realm> {
-        override fun onChange(element: Realm) {
-            reloadListView()
-        }
-    }
-
-
-
-    private lateinit var mTaskAdapter: TaskAdapter
-*/
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -151,62 +213,8 @@ class MainActivity : AppCompatActivity() {
 
         realm.close()
     }
-}
-
-/*
 
 
-    fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-
-        // Realmの設定
-        mRealm = Realm.getDefaultInstance()
-        mRealm.addChangeListener(mRealmListener )
-
-        // ListViewの設定
-       // mTaskAdapter = TaskAdapter(this@MainActivity)
-                mTaskAdapter = TaskAdapter(this@MainActivity)
-
-                // ListViewをタップしたときの処理
-        listView1.setOnItemClickListener { parent, view, position, id ->
-            // 入力・編集する画面に遷移させる
-        }
-
-        // ListViewを長押ししたときの処理
-        listView1.setOnItemLongClickListener { parent, view, position, id ->
-            // タスクを削除する
-            true
-        }
-
-        reloadListView()
-    }
-
-    private fun reloadListView() {
-        // Realmデータベースから、「全てのデータを取得して新しい日時順に並べた結果」を取得
-        val taskRealmResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
-
-        // 上記の結果を、TaskList としてセットする
-        mTaskAdapter.taskList = mRealm.copyFromRealm(taskRealmResults)
-
-        // TaskのListView用のアダプタに渡す
-        listView1.adapter = mTaskAdapter
-
-        // 表示を更新するために、アダプターにデータが変更されたことを知らせる
-        mTaskAdapter.notifyDataSetChanged()
-    }
-    */
-    /*
-    private fun reloadListView() {
-        // 後でTaskクラスに変更する
-        val taskList = mutableListOf("aaa", "bbb", "ccc")
-
-        mTaskAdapter.taskList = taskList
-        listView1.adapter = mTaskAdapter
-        mTaskAdapter.notifyDataSetChanged()
-    }
-    */
 
     override fun onDestroy() {
         super.onDestroy()
@@ -223,54 +231,6 @@ class MainActivity : AppCompatActivity() {
         mRealm.beginTransaction()
         mRealm.copyToRealmOrUpdate(task)
         mRealm.commitTransaction()
-    }
-}
-
-/*
-
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
-}
-
-*/
-
-/*
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
 */
