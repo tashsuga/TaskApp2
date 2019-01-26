d5de5f2ca429fe332488e152318ae3a8f166edf8
takeshisugai <tsu.vasco@gmail.com>
2019-01-26 16:42:41 +0900

    1/26

--- app/src/main/java/com/onespan/takeshisugai/taskapp2/TaskAdapter.kt
+++ app/src/main/java/com/onespan/takeshisugai/taskapp2/TaskAdapter.kt
@@ -13,7 +13,6 @@ import java.util.*
 
 
 //class TaskAdapter: BaseAdapter() {
-
 class TaskAdapter(context: Context): BaseAdapter() {
     private val mLayoutInflater: LayoutInflater
     var taskList = mutableListOf<Task>()
@@ -34,26 +33,6 @@ class TaskAdapter(context: Context): BaseAdapter() {
         return taskList[position].id.toLong()
     }
 
-    /*
-
-    init {
-        this.mLayoutInflater = LayoutInflater.from(context)
-    }
-
-    override fun getCount(): Int {
-        return taskList.size
-    }
-
-    override fun getItem(position: Int): Any {
-        return taskList[position]
-    }
-
-    override fun getItemId(position: Int): Long {
-        return 0
-    }
-
-    */
-
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view: View = convertView ?: mLayoutInflater.inflate(android.R.layout.simple_list_item_2, null)
 
@@ -68,55 +47,5 @@ class TaskAdapter(context: Context): BaseAdapter() {
 
         return view
     }
-    /*
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: View = convertView ?: mLayoutInflater.inflate(android.R.layout.simple_list_item_2, null)
-
-        val textView1 = view.findViewById<TextView>(android.R.id.text1)
-        val textView2 = view.findViewById<TextView>(android.R.id.text2)
-
-        // 後でTaskクラスから情報を取得するように変更する
-        textView1.text = taskList[position]
-
-        return view
-    }
-    */
-    /*
-        var taskList = mutableListOf<String>()
-
-        override fun getCount(): Int {
-            return taskList.size
-        }
-
-        override fun getItem(position: Int): Any {
-            return taskList[position]
-        }
-
-        override fun getItemId(position: Int): Long {
-            return 0
-        }
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-        */
-/*
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getItem(position: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getItemId(position: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-    */
-
 }
+
