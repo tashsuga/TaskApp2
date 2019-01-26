6575f6bcc9aab124b962cc0302520944e453fcc8
takeshisugai <tsu.vasco@gmail.com>
2019-01-26 21:30:27 +0900

    1/26

--- app/src/main/java/com/onespan/takeshisugai/taskapp2/InputActivity.kt
+++ app/src/main/java/com/onespan/takeshisugai/taskapp2/InputActivity.kt
@@ -14,30 +14,6 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Intent
 
-/*
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.app.DatePickerDialog
-import android.app.TimePickerDialog
-import android.support.v7.widget.Toolbar
-import android.view.View
-import io.realm.Realm
-import kotlinx.android.synthetic.main.content_input.*
-import java.util.*
-*/
-
-/*
-class InputActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input)
-    }
-}
-*/
-
-//const fun getEXTRA_TASK() = "com.onespan.takeshisugai.taskapp2.TASK"
-
 
 
 class InputActivity : AppCompatActivity() {
