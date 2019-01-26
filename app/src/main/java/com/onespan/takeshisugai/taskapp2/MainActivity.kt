8f2b331777c474d242dc61659e5cd8bc9aaea43d
takeshisugai <tsu.vasco@gmail.com>
2019-01-26 16:03:10 +0900

    1/26

--- app/src/main/java/com/onespan/takeshisugai/taskapp2/MainActivity.kt
+++ app/src/main/java/com/onespan/takeshisugai/taskapp2/MainActivity.kt
@@ -7,13 +7,52 @@ import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
 import io.realm.RealmChangeListener
 import io.realm.Sort
-
+import java.util.*
 
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mRealm: Realm
+
+    private var mYear = 0
+    private var mMonth = 0
+    private var mDay = 0
+    private var mHour = 0
+    private var mMinute = 0
+    private var mTask: Task? = null
+
+    private val mOnDateClickListener = View.OnClickListener {
+        val datePickerDialog = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                mYear = year
+                mMonth = month
+                mDay = dayOfMonth
+                val dateString = mYear.toString() + "/" + String.format("%02d", mMonth + 1) + "/" + String.format("%02d", mDay)
+                date_button.text = dateString
+            }, mYear, mMonth, mDay)
+        datePickerDialog.show()
+    }
+
+    private la private val mOnTimeClickListener = View.OnClickListener {
+        val timePickerDialog = TimePickerDialog(this,
+            TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+                mHour = hour
+                mMinute = minute
+                val timeString = String.format("%02d", mHour) + ":" + String.format("%02d", mMinute)
+                times_button.text = timeString
+            }, mHour, mMinute, false)
+        timePickerDialog.show()
+    }
+
+    private val mOnDoneClickListener = View.OnClickListener {
+        addTask()
+        finish()
+    }
+
+
+    /*
+    teinit var mRealm: Realm
 
     private val mRealmListener = object : RealmChangeListener<Realm> {
         override fun onChange(element: Realm) {
@@ -24,12 +63,100 @@ class MainActivity : AppCompatActivity() {
 
 
     private lateinit var mTaskAdapter: TaskAdapter
+*/
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fab.setOnClickListener { view ->
+        // ActionBarを設定する
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        if (supportActionBar != null) {
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        }
+
+        // UI部品の設定
+        date_button.setOnClickListener(mOnDateClickListener)
+        times_button.setOnClickListener(mOnTimeClickListener)
+        done_button.setOnClickListener(mOnDoneClickListener)
+
+        // EXTRA_TASK から Task の id を取得して、 id から Task のインスタンスを取得する
+        val intent = intent
+        val taskId = intent.getIntExtra(EXTRA_TASK, -1)
+        val realm = Realm.getDefaultInstance()
+        mTask = realm.where(Task::class.java).equalTo("id", taskId).findFirst()
+        realm.close()
+
+        if (mTask == null) {
+            // 新規作成の場合
+            val calendar = Calendar.getInstance()
+            mYear = calendar.get(Calendar.YEAR)
+            mMonth = calendar.get(Calendar.MONTH)
+            mDay = calendar.get(Calendar.DAY_OF_MONTH)
+            mHour = calendar.get(Calendar.HOUR_OF_DAY)
+            mMinute = calendar.get(Calendar.MINUTE)
+        } else {
+            // 更新の場合
+            title_edit_text.setText(mTask!!.title)
+            content_edit_text.setText(mTask!!.contents)
+
+            val calendar = Calendar.getInstance()
+            calendar.time = mTask!!.date
+            mYear = calendar.get(Calendar.YEAR)
+            mMonth = calendar.get(Calendar.MONTH)
+            mDay = calendar.get(Calendar.DAY_OF_MONTH)
+            mHour = calendar.get(Calendar.HOUR_OF_DAY)
+            mMinute = calendar.get(Calendar.MINUTE)
+
+            val dateString = mYear.toString() + "/" + String.format("%02d", mMonth + 1) + "/" + String.format("%02d", mDay)
+            val timeString = String.format("%02d", mHour) + ":" + String.format("%02d", mMinute)
+
+            date_button.text = dateString
+            times_button.text = timeString
+        }
+    }
+    private fun addTask() {
+        val realm = Realm.getDefaultInstance()
+
+        realm.beginTransaction()
+
+        if (mTask == null) {
+            // 新規作成の場合
+            mTask = Task()
+
+            val taskRealmResults = realm.where(Task::class.java).findAll()
+
+            val identifier: Int =
+                if (taskRealmResults.max("id") != null) {
+                    taskRealmResults.max("id")!!.toInt() + 1
+                } else {
+                    0
+                }
+            mTask!!.id = identifier
+        }
+
+        val title = title_edit_text.text.toString()
+        val content = content_edit_text.text.toString()
+
+        mTask!!.title = title
+        mTask!!.contents = content
+        val calendar = GregorianCalendar(mYear, mMonth, mDay, mHour, mMinute)
+        val date = calendar.time
+        mTask!!.date = date
+
+        realm.copyToRealmOrUpdate(mTask!!)
+        realm.commitTransaction()
+
+        realm.close()
+    }
+}
+
+/*
+
+
+    fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
@@ -69,6 +196,7 @@ class MainActivity : AppCompatActivity() {
         // 表示を更新するために、アダプターにデータが変更されたことを知らせる
         mTaskAdapter.notifyDataSetChanged()
     }
+    */
     /*
     private fun reloadListView() {
         // 後でTaskクラスに変更する
