b32bc52d45a521775e4a5e3707fffe0f8d641f48
takeshisugai <tsu.vasco@gmail.com>
2019-01-26 18:31:06 +0900

    1/26

---
+++ app/src/main/java/com/onespan/takeshisugai/taskapp2/TaskAlarmReceiver.kt
@@ -0,0 +1,74 @@
+package com.onespan.takeshisugai.taskapp2
+
+//package jp.techacademy.taro.kirameki.taskapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.View
+import io.realm.Realm
+
+import java.util.*
+import android.app.AlarmManager
+import android.app.PendingIntent
+
+
+class TaskAlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.d("TaskApp", "onReceive")
+    }
+
+
+    private fun addTask() {
+        val realm = Realm.getDefaultInstance()
+
+        realm.beginTransaction()
+
+        if (mTask == null) {
+            // 新規作成の場合
+            mTask = Task()
+
+            val taskRealmResults = realm.where(Task::class.java).findAll()
+
+            val identifier: Int =
+                if (taskRealmResults.max("id") != null) {
+                    taskRealmResults.max("id")!!.toInt() + 1
+                } else {
+                    0
+                }
+            mTask!!.id = identifier
+        }
+
+        val title = title_edit_text.text.toString()
+        val content = content_edit_text.text.toString()
+
+        mTask!!.title = title
+        mTask!!.contents = content
+        val calendar = GregorianCalendar(mYear, mMonth, mDay, mHour, mMinute)
+        val date = calendar.time
+        mTask!!.date = date
+
+        realm.copyToRealmOrUpdate(mTask!!)
+        realm.commitTransaction()
+
+        realm.close()
+
+        val resultIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
+        resultIntent.putExtra(EXTRA_TASK, mTask!!.id)
+        val resultPendingIntent = PendingIntent.getBroadcast(
+            this,
+            mTask!!.id,
+            resultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, resultPendingIntent)
+    }
+}
